fe3d642c00dd0cb082ed1977a38b7943eace93a1
shgamestudio <45031813+shgamestudio@users.noreply.github.com>
2018-12-09 11:44:19 +0700

    Hoàn thành intro

---
+++ fade_editfix2/fade/Begin.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace fade
+{
+    partial class Begin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Begin));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timerTalk = new System.Windows.Forms.Timer(this.components);
+            this.timerTalk2 = new System.Windows.Forms.Timer(this.components);
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer2 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.axWindowsMediaPlayer_YS = new AxWMPLib.AxWindowsMediaPlayer();
+            this.pictureHinh2 = new System.Windows.Forms.PictureBox();
+            this.pictureYS_run = new System.Windows.Forms.PictureBox();
+            this.pictureIre = new System.Windows.Forms.PictureBox();
+            this.pictureNoxus = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.pictureHinh = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer_YS)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHinh2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureYS_run)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIre)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNoxus)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHinh)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Cambria", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(170, 618);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1009, 126);
+            this.label1.TabIndex = 0;
+            // 
+            // timerTalk
+            // 
+            this.timerTalk.Interval = 20;
+            this.timerTalk.Tick += new System.EventHandler(this.timerTalk_Tick);
+            // 
+            // timerTalk2
+            // 
+            this.timerTalk2.Interval = 90;
+            this.timerTalk2.Tick += new System.EventHandler(this.timerTalk2_Tick);
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(1032, 24);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 2;
+            this.axWindowsMediaPlayer1.Visible = false;
+            // 
+            // axWindowsMediaPlayer2
+            // 
+            this.axWindowsMediaPlayer2.Enabled = true;
+            this.axWindowsMediaPlayer2.Location = new System.Drawing.Point(1032, 85);
+            this.axWindowsMediaPlayer2.Name = "axWindowsMediaPlayer2";
+            this.axWindowsMediaPlayer2.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer2.OcxState")));
+            this.axWindowsMediaPlayer2.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer2.TabIndex = 5;
+            this.axWindowsMediaPlayer2.Visible = false;
+            // 
+            // axWindowsMediaPlayer_YS
+            // 
+            this.axWindowsMediaPlayer_YS.Enabled = true;
+            this.axWindowsMediaPlayer_YS.Location = new System.Drawing.Point(-1, 0);
+            this.axWindowsMediaPlayer_YS.Name = "axWindowsMediaPlayer_YS";
+            this.axWindowsMediaPlayer_YS.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer_YS.OcxState")));
+            this.axWindowsMediaPlayer_YS.Size = new System.Drawing.Size(1192, 612);
+            this.axWindowsMediaPlayer_YS.TabIndex = 8;
+            this.axWindowsMediaPlayer_YS.Visible = false;
+            // 
+            // pictureHinh2
+            // 
+            this.pictureHinh2.BackColor = System.Drawing.Color.Transparent;
+            this.pictureHinh2.Enabled = false;
+            this.pictureHinh2.Location = new System.Drawing.Point(-1, 0);
+            this.pictureHinh2.Name = "pictureHinh2";
+            this.pictureHinh2.Size = new System.Drawing.Size(1193, 613);
+            this.pictureHinh2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureHinh2.TabIndex = 10;
+            this.pictureHinh2.TabStop = false;
+            this.pictureHinh2.Visible = false;
+            // 
+            // pictureYS_run
+            // 
+            this.pictureYS_run.Enabled = false;
+            this.pictureYS_run.Image = ((System.Drawing.Image)(resources.GetObject("pictureYS_run.Image")));
+            this.pictureYS_run.Location = new System.Drawing.Point(413, 103);
+            this.pictureYS_run.Name = "pictureYS_run";
+            this.pictureYS_run.Size = new System.Drawing.Size(356, 422);
+            this.pictureYS_run.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureYS_run.TabIndex = 9;
+            this.pictureYS_run.TabStop = false;
+            this.pictureYS_run.Visible = false;
+            // 
+            // pictureIre
+            // 
+            this.pictureIre.Image = ((System.Drawing.Image)(resources.GetObject("pictureIre.Image")));
+            this.pictureIre.Location = new System.Drawing.Point(-1, 0);
+            this.pictureIre.Name = "pictureIre";
+            this.pictureIre.Size = new System.Drawing.Size(120, 74);
+            this.pictureIre.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureIre.TabIndex = 7;
+            this.pictureIre.TabStop = false;
+            // 
+            // pictureNoxus
+            // 
+            this.pictureNoxus.Image = ((System.Drawing.Image)(resources.GetObject("pictureNoxus.Image")));
+            this.pictureNoxus.Location = new System.Drawing.Point(0, 0);
+            this.pictureNoxus.Name = "pictureNoxus";
+            this.pictureNoxus.Size = new System.Drawing.Size(210, 138);
+            this.pictureNoxus.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureNoxus.TabIndex = 6;
+            this.pictureNoxus.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(10, 618);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(154, 126);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button3.CausesValidation = false;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Location = new System.Drawing.Point(1005, 553);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(187, 62);
+            this.button3.TabIndex = 3;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Visible = false;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            this.button3.MouseEnter += new System.EventHandler(this.button3_MouseEnter);
+            this.button3.MouseLeave += new System.EventHandler(this.button3_MouseLeave);
+            // 
+            // pictureHinh
+            // 
+            this.pictureHinh.BackColor = System.Drawing.Color.Transparent;
+            this.pictureHinh.Image = ((System.Drawing.Image)(resources.GetObject("pictureHinh.Image")));
+            this.pictureHinh.Location = new System.Drawing.Point(-1, 0);
+            this.pictureHinh.Name = "pictureHinh";
+            this.pictureHinh.Size = new System.Drawing.Size(1193, 613);
+            this.pictureHinh.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureHinh.TabIndex = 1;
+            this.pictureHinh.TabStop = false;
+            // 
+            // Begin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1192, 751);
+            this.Controls.Add(this.pictureHinh2);
+            this.Controls.Add(this.pictureYS_run);
+            this.Controls.Add(this.axWindowsMediaPlayer_YS);
+            this.Controls.Add(this.pictureIre);
+            this.Controls.Add(this.pictureNoxus);
+            this.Controls.Add(this.axWindowsMediaPlayer2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.pictureHinh);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Begin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Begin";
+            this.Load += new System.EventHandler(this.Begin_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer_YS)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHinh2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureYS_run)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureIre)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureNoxus)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureHinh)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timerTalk;
+        private System.Windows.Forms.PictureBox pictureHinh;
+        private System.Windows.Forms.Timer timerTalk2;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer2;
+        private System.Windows.Forms.PictureBox pictureNoxus;
+        private System.Windows.Forms.PictureBox pictureIre;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer_YS;
+        private System.Windows.Forms.PictureBox pictureYS_run;
+        private System.Windows.Forms.PictureBox pictureHinh2;
+    }
+}
---
+++ fade_editfix2/fade/Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+
+namespace fade
+{
+    public partial class Form1 : Form
+    {
+        int play = 0;
+        private SoundPlayer _soundPlayer1; //Biến âm thanh
+
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            _soundPlayer1 = new SoundPlayer("SHvoice.wav");
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.Opacity > 0.5)
+                this.Opacity -= 0.015;
+            
+            else
+            {
+                
+                timer1.Stop();
+
+                //using (Form2 fm = new Form2())
+                //{
+
+                //    fm.ShowDialog();
+
+                //}
+                new Form2().Show();
+                this.Hide();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _soundPlayer1.Play();
+            pictureBox1.Image = Properties.Resources.SHG_right;
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            // auto next scence
+           
+                timer2.Start();
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (play<5)
+                play++;
+            else
+            {
+                timer2.Stop();
+                timer1.Start();
+            }
+        }
+    }
+}
