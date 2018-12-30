b930182330517a6411df5f55546073239961a88c
SH studio <>
2018-12-30 10:29:54 +0700

    08 đang làm

---
+++ QUANLIBANHANG/QUANLIBANHANG/DAL/TableDAL.cs
@@ -0,0 +1,22 @@
+﻿using QUANLIBANHANG.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUANLIBANHANG.DAL
+{
+    class TableDAL
+    {
+        private static TableDAL instance;
+        private TableDAL() { }
+        internal static TableDAL Instance { get => instance; set => instance = value; }
+        public List<Table> LoadTableList()
+        {
+            List<Table> tables = new List<Table>();
+
+            return tables;
+        }
+    }
+}
---
+++ QUANLIBANHANG/QUANLIBANHANG/DTO/Table.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QUANLIBANHANG.DTO
+{
+    class Table
+    {
+        private string name;
+        private int iD;
+        private string status;
+
+        private Table(int id, string name, string status)
+        {
+            ID = id;
+            Name = name;
+            Status = status;
+        }
+
+        public int ID { get => iD; set => iD = value; }
+        public string Name { get => Name; set => Name = value; }
+        public string Status { get => status; set => status = value; }
+    }
+}
